8a7e5b7b317495abd5ce4bac9792acae61649a2c
Alexander Santos <alexanderferreira@gmail.com>
2019-03-31 16:00:15 -0300

    3 TP de POO

---
+++ src/java/br/com/fatecpg/cadastro/Clientes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.cadastro;
+
+/**
+ *
+ * @author alexandersantos
+ */
+public class Clientes {
+    private String nomecliente;
+    private String cpf;
+    private String rg;
+    private String emailcliente;
+    private String telefonecliente;
+    private String enderecocliente;
+}
---
+++ src/java/br/com/fatecpg/cadastro/Fornecedores.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.cadastro;
+
+/**
+ *
+ * @author alexandersantos
+ */
+public class Fornecedores {
+    private String nomefornecedor;
+    private String razao;
+    private String cnpj;
+    private String emailfornecedor;
+    private String telefonefornecedor;
+    private String enderecofornecedor;
+}
